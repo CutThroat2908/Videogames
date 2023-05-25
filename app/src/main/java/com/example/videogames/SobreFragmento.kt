1e628d20e8598779d4006362595b194735cf5769
CutThroat2908 <rnpaula2000@gmail.com>
2023-05-25 23:09:18 +0100

    Recuperação da aula anterior

--- app/src/main/java/com/example/videogames/SobreFragmento.kt
+++ app/src/main/java/com/example/videogames/SobreFragmento.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.videogames.databinding.FragmentSecondBinding
+import com.example.videogames.databinding.FragmentoSobreBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SobreFragmento : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentoSobreBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentoSobreBinding.inflate(inflater, container, false)
         return binding.root
 
     }
--- app/src/main/java/com/example/videogames/VideogamesContentProvider.kt
+++ app/src/main/java/com/example/videogames/VideogamesContentProvider.kt
@@ -5,8 +5,9 @@ import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
+import android.provider.BaseColumns
 
-class InstrumentosContentProvider : ContentProvider() {
+class VideogamesContentProvider : ContentProvider() {
 
     private var bdOpenHelper : BDJogosOpenHelper?=null
 
@@ -17,45 +18,135 @@ class InstrumentosContentProvider : ContentProvider() {
     }
 
     override fun query(
-        p0: Uri,
-        p1: Array<out String>?,
-        p2: String?,
-        p3: Array<out String>?,
-        p4: String?
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
     ): Cursor? {
-        TODO("Not yet implemented")
+        val db = bdOpenHelper!!.readableDatabase
+        val id =uri.lastPathSegment
+
+        val endereco = uriMatcher().match(uri)
+
+
+        val tabela = when (endereco){
+            URI_JOGADORES, URI_JOGADOR_ID -> TabelaJogadores(db)
+            URI_JOGOS, URI_JOGO_ID -> TabelaJogos(db)
+            else -> null
+
+        }
+
+        var (selecao, argsSelecao) = when (endereco){
+            URI_JOGADOR_ID, URI_JOGO_ID -> Pair("${BaseColumns._ID}=?", arrayOf(id))
+            else -> Pair(selection,selectionArgs)
+        }
+
+        return tabela?.consulta(
+            projection as Array<String>,
+            selecao ,
+            argsSelecao as Array<String>,
+            null,null,
+            sortOrder)
     }
 
 
+    override fun getType(uri: Uri): String? {
+        val endereco = uriMatcher().match(uri)
 
-    override fun getType(p0: Uri): String? {
-        TODO("Not yet implemented")
+        return when(endereco){
+
+            URI_JOGOS-> "vdn.android.cursor.dir/$JOGOS"
+            URI_JOGADORES -> "vdn.android.cursor.dir/$JOGADORES"
+            URI_JOGO_ID -> "vdn.android.cursor.item/$JOGOS"
+            URI_JOGADOR_ID -> "vdn.android.cursor.item/$JOGADORES"
+            else -> null
+
+        }
+
     }
 
-    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
-        TODO("Not yet implemented")
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        val db = bdOpenHelper!!.writableDatabase
+
+        val endereco = uriMatcher().match(uri)
+
+        val tabela = when (endereco){
+            URI_JOGADORES -> TabelaJogadores(db)
+            URI_JOGOS -> TabelaJogos(db)
+            else -> return null
+
+        }
+
+        val id = tabela.insere(values!!)
+        if(id == -1L){
+            return null
+        }
+
+        return Uri.withAppendedPath(uri,id.toString())
     }
 
-    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
-        TODO("Not yet implemented")
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+
+
+        val db = bdOpenHelper!!.writableDatabase
+
+        val endereco = uriMatcher().match(uri)
+
+        val tabela = when (endereco) {
+            URI_JOGADOR_ID -> TabelaJogadores(db)
+            URI_JOGO_ID -> TabelaJogos(db)
+            else -> return 0
+
+        }
+
+        val id = uri.lastPathSegment!!
+
+        return tabela.elimina( "${BaseColumns._ID}=?", arrayOf(id))
     }
 
-    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
-        TODO("Not yet implemented")
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?):
+            Int {
+
+        val db = bdOpenHelper!!.writableDatabase
+
+        val endereco = uriMatcher().match(uri)
+
+        val tabela = when (endereco){
+            URI_JOGADOR_ID -> TabelaJogadores(db)
+            URI_JOGO_ID -> TabelaJogos(db)
+            else -> return 0
+
+        }
+
+        val id = uri.lastPathSegment!!
+
+        return tabela.altera(values!!,"${BaseColumns._ID}=?", arrayOf(id))
+
     }
 
     companion object{
 
         private const val AUTORIDADE ="com.example.videogames"
         const val JOGOS = "jogos"
-        const val JOGADORES = "jogadores"
-        const val URI_JOGOS = 200
-        const val URI_JOGADORES = 100
+        const val JOGADORES="jogadores"
+        private const val URI_JOGO_ID=200
+        private const val URI_JOGADOR_ID =101
+        private const val URI_JOGADORES=100
+        private const val URI_JOGOS =201
 
         fun uriMatcher()=UriMatcher(UriMatcher.NO_MATCH).apply {
             addURI(AUTORIDADE, JOGADORES, URI_JOGADORES)
+            addURI(AUTORIDADE, "$JOGADORES/#", URI_JOGADOR_ID)
             addURI(AUTORIDADE, JOGOS, URI_JOGOS)
+            addURI(AUTORIDADE, "$JOGOS/#", URI_JOGO_ID)
         }
+
     }
 
 
